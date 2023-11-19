091232e4da5de8da6bd248fd67e4b04fb85beb58
Giovanna <giovannaevangelista78@gmail.com>
2023-11-19 16:10:05 -0300

    hotfix interest

--- src/ONG/get_ong_interests/get_ong_interests.service.ts
+++ src/ONG/get_ong_interests/get_ong_interests.service.ts
@@ -6,15 +6,15 @@ import { ONG } from 'src/schemas/ong_schema';
 import { Product } from 'src/schemas/product_schema';
 
 @Injectable()
-export class GetONGInterestsService{
+export class GetONGInterestsService {
   constructor(
     @InjectModel(ONG.name)
     private ONGModel: Model<ONG>,
-  ) {}
+  ) { }
 
 
   async findONGInterests(id: string): Promise<Product[]> {
-    const foundONG = await this.ONGModel.findById(id).populate("produtos").exec();
+    const foundONG = await this.ONGModel.findById(id).populate("interesses").exec();
     return foundONG.interesses
   }
 }
