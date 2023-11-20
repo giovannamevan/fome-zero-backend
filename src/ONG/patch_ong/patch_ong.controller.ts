f01db83db13f508cd5b79f3d9df579ef006da384
SimonnSouza <114405726+SimonnSouza@users.noreply.github.com>
2023-11-19 21:47:55 -0300

    Update patch_ong.controller.ts

--- src/ONG/patch_ong/patch_ong.controller.ts
+++ src/ONG/patch_ong/patch_ong.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Post,  } from "@nestjs/common";
+import { Controller, Body, Post, Patch  } from "@nestjs/common";
 import { NewONGDto } from "src/DTO´s/new_ong.dto";
 import { EditONGService } from "./patch_ong.service";
 import { EditONGDto } from "src/DTO´s/edit_ong.dto";
@@ -7,7 +7,7 @@ import { EditONGDto } from "src/DTO´s/edit_ong.dto";
 export class EditONGController{
     constructor(private readonly useEditONG:EditONGService) { }
 
-    @Post('/')
+    @Patch('/')
     async PostUser(@Body()newONGDto:EditONGDto )
     {
     const newDonatorCreated = await this.useEditONG.editONG(newONGDto)
