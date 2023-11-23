8fc079130282474c4fab481a70004d573d42a3e6
Henrick Oliveira <simonnsouza10@hotmail.com>
2023-11-23 10:40:51 -0300

    fix delete interest

--- src/ONG/desist_product/desist_product.controller.ts
+++ src/ONG/desist_product/desist_product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param } from "@nestjs/common"
+import { Controller, Delete, Param } from "@nestjs/common"
 import { ONG } from "src/schemas/ong_schema"
 import { GiveUpOnProductService } from "./desist_product.service"
 
@@ -6,8 +6,8 @@ import { GiveUpOnProductService } from "./desist_product.service"
 export class GiveUpProductController{
     constructor(private readonly useGiveUp:GiveUpOnProductService) { }
 
-    @Delete('/:ong_id;/:product_id')
-    async GetDonatorById(@Param("ong_id") ongId:string, @Param("product_id") productId:string):Promise<ONG> {
+    @Delete('/:ong_id/:product_id')
+    async DeleteProductFromInterest(@Param("ong_id") ongId:string, @Param("product_id") productId:string):Promise<ONG> {
         const deletedProduct = await this.useGiveUp.GiveUpOnProduct(productId,ongId)
         return
     }
